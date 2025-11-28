b20c197f45e8500370ae327161d835e6a4a98fa7
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-11-28 09:17:40 -0800

    completed chapter 5 Q9

---
+++ JavaScript Assignment-Chapter-05-to-09/app.js
@@ -0,0 +1,135 @@
+
+// -------------------CHAPTER 05 (MATH EXPRESSIONS)----------------------------------------
+
+// -------CHAPTER 05 QUESTION (1)---------
+
+// var num1 = 3;
+// var num2 = 5;
+// var total = num1 + num2;
+// console.log("Two values are: " + num1 + " and "+  num2)
+// console.log("The sum of numbers is : "+total);
+
+// -------CHAPTER 05 QUESTION (2)---------
+
+// console.log("Two values are: " + num1 + " and "+  num2)
+
+// var total = num1 * num2;
+// console.log("The Muiltiplication of two number is: "+total);
+// var total = num1 / num2;
+// console.log("The Division of two number is: "+total);
+// var total = num1 - num2;
+// console.log("Subtraction of two number is: "+total);
+// var total = num1 % num2;
+// console.log("Modules of two number is: "+total);
+
+
+// -------CHAPTER 05 QUESTION (3)---------
+
+// var myNumber ;
+// console.log("value after declaration :" + myNumber);
+// var myNumber = 3 + 2;
+// console.log("initial Value : "+ myNumber);
+
+// myNumber++;
+// console.log("Value after increment : " + myNumber)
+
+//  myNumber = myNumber + 7;
+
+//  console.log("value after adding 7 is: " + myNumber)
+
+//  myNumber--;
+
+//  console.log("value after decrement is : "+ myNumber)
+
+//  var remainder = myNumber % 3;
+  
+//  console.log("final Remainder of value is: "+ remainder)
+
+
+// -------CHAPTER 05 QUESTION (4)---------
+
+// var ticketPrice = 600;
+// var ticketPurchased = 5;
+
+// var totalAmount =  ticketPrice * ticketPurchased;
+
+// console.log("Total cost to buy "+ ticketPurchased + " tickets to a movie is " + totalAmount + " PKR.")
+
+
+// -------CHAPTER 05 QUESTION (5)---------
+
+// var tableNumber =4;
+
+// console.log("Table of " + tableNumber + " is following:" )
+// console.log(tableNumber + " X  1 = " + (tableNumber*1));
+// console.log(tableNumber + " X  2 = " + (tableNumber*2));
+// console.log(tableNumber + " X  3 = " + (tableNumber*3));
+// console.log(tableNumber + " X  4 = " + (tableNumber*4));
+// console.log(tableNumber + " X  5 = " + (tableNumber*5));
+// console.log(tableNumber + " X  6 = " + (tableNumber*6));
+// console.log(tableNumber + " X  7 = " + (tableNumber*7));
+// console.log(tableNumber + " X  8 = " + (tableNumber*8));
+// console.log(tableNumber + " X  9 = " + (tableNumber*9));
+// console.log(tableNumber + " X  10 = " + (tableNumber*10));
+
+
+// -------CHAPTER 05 QUESTION (6)---------
+
+// ----------Temperature Converter---------------------------
+
+// ------converted to fahrenhite ----------
+// var celsius = 40;
+// console.log("Converting Temperature  from ceslius (40deg) to fahrenhite");
+// fahrenhite = (celsius*9/5)+32;
+// console.log("40 degree celsius is equal to : " + fahrenhite + " fahrenhite")
+
+// --------converted to celsius--------------
+// fahrenhite = 104;
+// console.log("converting " + fahrenhite  + " fahrenhite  to degree celsius!")
+// var celsius = (fahrenhite-32)*5/9;
+// console.log("converted 104 fahrenhite to celsius is: " + celsius )
+
+
+// -------CHAPTER 05 QUESTION (7)---------
+
+// var item1Price = 650;
+// var item2Price = 100;
+// var item1Quantity =3;
+// var item2Quantity =7;
+// var shipmentCharge =100;
+
+// console.log("Price of item one : " + item1Price)
+// console.log("Quantity of item one : " + item1Quantity)
+// console.log("Price of item two : " + item2Price)
+// console.log("Quantity of item two : " + item2Quantity)
+// console.log("Shipping Charges  : " + shipmentCharge)
+// var totalCost = (item1Price*item1Quantity) + (item2Price*item2Quantity) + shipmentCharge;
+// console.log("Total cost fo your order is : " + totalCost)
+
+
+// -------CHAPTER 05 QUESTION (8)---------
+
+// ---------MARK SHEET---------------
+
+// var obtained_mark = 804;
+// var totalMark=980;
+// console.log("Total Number : " +  totalMark);
+// console.log("Obtaining Mark : " + obtained_mark)
+// var percentage = (obtained_mark/totalMark)*100;
+// console.log("Percentage of marks is: " + percentage)
+
+
+// -------CHAPTER 05 QUESTION (9)---------
+
+// var usDollerRate = 104.80;
+// var usDollerCount = 10;
+// var saudiRiyalRate = 28;
+// var saudiRiyalCount=25;
+
+// var usCash = (usDollerRate*usDollerCount);
+// var saudiCash = (saudiRiyalRate*saudiRiyalCount);
+// console.log("Total Us Dollers : "+ usCash);
+// console.log("Total Saudi Riyal Converted : " + saudiCash)
+// console.log("Totol Cash Convertion " + (usCash + saudiCash) );
+
+
