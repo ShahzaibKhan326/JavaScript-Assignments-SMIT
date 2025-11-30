d6ee3a18449a2b1654ccaa354ce4acccde0511ee
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-11-30 06:48:52 -0800

    completed chapter 02 and 01

---
+++ JavaScript Assignment/app.js
@@ -0,0 +1,98 @@
+
+//chapter 1  ( Q1 )
+
+// alert("welcome")
+
+//chapter 1  ( Q2 )
+
+// alert("Error Please enter a valid password")
+
+//chapter 1  ( Q3 )
+
+// alert("Welcome to Js Land... \n Happy Coding")
+
+//chapter 1  ( Q4 )
+
+// alert("welcome to js Land");
+// alert("Happy Coding")
+
+//chapter 1  ( Q5 )
+
+// console.log(alert("Hello I can run Js through my web browswer console"))
+
+//chapter 1  ( Q6 )
+
+// alert("I am Using alert in my Assignment of JavaScript")
+
+//chapter 1  ( Q7 )
+
+// alert("for Answer Q7 of Chapter ! go to index.html")
+
+
+// ----------------------(CHAPTER 02-----------------------------------
+
+//chapter 2 ( Q1 )
+
+var userName;
+
+//chapter 2  ( Q2 )
+
+var myName ="shahzaib";
+
+var firstName = "shahzaib ";
+var lastName = "khan";
+var fullName = firstName + lastName;
+console.log(fullName);
+
+//chapter 2  ( Q3 )
+
+// var message;
+// message ="Hello World!";
+// alert(message);
+
+//chapter 2  ( Q4 )
+ 
+var name="shahzaib khan";
+var age="22";
+var certification ="Certified Mobile Application Development"
+
+// alert(name);
+// alert(age);
+// alert(certification);
+
+//chapter 2  ( Q5 )
+
+// alert("PIZZA");
+// alert("PIZZ");
+// alert("PIZ");
+// alert("PI");
+// alert("P");
+
+//chapter 2  ( Q6 )
+
+var MyEmail = "xyz@gmail.com";
+// alert ("My Email Address is : " + MyEmail);
+
+//chapter 2  ( Q7 )
+
+var Book ="A smarter way to learn JS";
+// alert("I am trying to learn from the Book : "+  Book);
+
+//chapter 2  ( Q8 )
+
+// document.write("Yah1 I can write HTML content through JavaScript");
+
+//chapter 2  ( Q9)
+
+// alert("꧁༒☬star☬༒꧂");
+
+
+
+
+
+
+
+
+
+
+
