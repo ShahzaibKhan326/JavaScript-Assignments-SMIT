65888d438113bd068b1d0a0148585ac5c17786a3
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-01 22:56:16 -0800

    completed Q2

---
+++ JavaScript Assignment-Chapter-12-to-13/app.js
@@ -0,0 +1,55 @@
+// ---------------Chatper (12-To-13)------------------------------------
+
+//--------------Question (1)-----------------------
+
+// var userInput = prompt("Enter any Character or Number to check");
+
+// var c = userInput.charAt(0)
+// var ascii = c.charCodeAt(0);
+// if(!userInput)
+// {
+//     console.log("Please enter a valid character")
+// }
+// else if(ascii >=48 && ascii <=57 )
+// {
+//     console.log("you entered a Number");
+// }
+// else if(ascii >=97 && ascii <=122)
+// {
+//     console.log("you entered a lower-case character")
+// }
+// else if(ascii >=65 && ascii <=90)
+// {
+//     console.log("you entered a upper-case character")
+// }
+// else
+// {
+//  console.log("you enter a symbol ");
+// }
+
+
+//--------------Question (2)-----------------------
+
+// var user_Number_01 = Number(prompt("Enter the first Number to compare :"))
+// var user_Number_02 = Number(prompt("Enter the second Number to compare :"))
+
+// if(!user_Number_01 || !user_Number_02  )
+// {
+//   console.log("Number are Missing! \nPlease enter valid number")
+// }
+// else if(user_Number_01 > user_Number_02)
+// {
+//     console.log(user_Number_01 +" is greater than " + user_Number_02);
+// }
+// else if(user_Number_02 > user_Number_01)
+// {
+//     console.log(user_Number_02 +" is greater than " + user_Number_01);
+    
+// }
+// else if(user_Number_01 === user_Number_02)
+// {
+//     console.log(user_Number_02 +" is greater than " + user_Number_02);
+    
+// }
+
+//--------------Question (3)-----------------------
