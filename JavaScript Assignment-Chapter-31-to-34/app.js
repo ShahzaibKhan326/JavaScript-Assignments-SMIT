4111609b8371939efb2ae860c29b6f7aab86c17b
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-10 11:22:20 -0800

    completed Q3

--- JavaScript Assignment-Chapter-31-to-34/app.js
+++ JavaScript Assignment-Chapter-31-to-34/app.js
@@ -7,4 +7,16 @@
 
 // -------------------Question (2)------------------------------
 
-                                                
+// var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// var my_Date =  new Date();
+// var my_Month = my_Date.getMonth();
+// console.log("Current month: "+months[my_Month])
+
+// -------------------Question (3)------------------------------
+
+// var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// var my_Date =  new Date();
+// var my_day = my_Date.getDay();
+// console.log("Today : "+ days[my_day])
