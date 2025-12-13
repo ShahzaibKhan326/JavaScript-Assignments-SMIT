a16cbd4ec307df5744ac9b4bfb2eb2376632f0ea
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-13 22:20:11 +0500

    completed Q1 and Q2

---
+++ JavaScript_Assignment-Chapter-34-to-38/app.js
@@ -0,0 +1,21 @@
+
+// -----------Chapter (34-To-38)-----------------------
+
+// -------------Question (1)---------------------------------
+
+// function showDate()
+// {
+//   var date= new Date();
+//   console.log(date);
+// }
+ 
+// showDate();
+
+// -------------Question (2)---------------------------------
+
+// function fullName(first_name,last_name)
+// {
+//   console.log(first_name + " "+ last_name)
+// }
+
+// fullName("shahzaib","khan")
