9c96d10d2c1755cd77890a37bede8684e549c4ec
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-13 22:26:29 +0500

    completed Q3

--- JavaScript_Assignment-Chapter-34-to-38/app.js
+++ JavaScript_Assignment-Chapter-34-to-38/app.js
@@ -19,3 +19,13 @@
 // }
 
 // fullName("shahzaib","khan")
+
+// -------------Question (3)---------------------------------
+
+// function add(num1,num2)
+// {
+//   console.log("Adding " + num1 + " and " + num2)
+//   console.log("Values after Adding : " + (num1+num2))
+// }
+
+// add(5,3);
