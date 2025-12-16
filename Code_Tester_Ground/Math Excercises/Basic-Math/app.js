bf6c11e10c17b35f3715dc173961154b826a4c68
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-16 01:06:42 -0800

    update

---
+++ Code_Tester_Ground/Math Excercises/Basic-Math/app.js
@@ -0,0 +1,54 @@
+// console.log("hello world!")
+
+// --------------Math Excersices-----------------------------
+
+// ---------Addition---------------
+// function add(num1,num2)
+// {
+//     return num1+num2;
+// }
+// console.log(add(2,5));
+
+// --------Subtraction----------------
+// function sub(num1,num2)
+// {
+//     return num1-num2;
+// }
+// console.log(sub(2,5));
+
+// ---------Muiltiplication---------------
+// function muilti(num1,num2)
+// {
+//     return num1*num2;
+// }
+// console.log(muilti(2,5));
+
+// ----------Division--------------
+// function division(num1,num2)
+// {
+//     return num1%num2;
+// }
+// console.log(division(2,5));
+
+// ----------Even or Odd--------------
+
+// function check_Odd_Even(num)
+// {
+//       if(num % 2 === 0 )
+//       {
+//         return num + " is Even Number";
+//       }
+//       else
+//       {
+//         return num + " is odd Number";
+//       }
+//       return message;
+// }
+// check_Odd_Even(2);
+
+// ----------Modules--------------
+// function Remainder(num1,num2)
+// {
+//     return num1/num2;
+// }
+// console.log(Remainder(2,5));
