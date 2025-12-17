a63e076c75009dde351fc4403c2df9eb42150722
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-16 21:02:38 -0800

    practice switch

---
+++ Code_Tester_Ground/Practice_Ground/app.js
@@ -0,0 +1,24 @@
+const expr = "Papayas";
+switch (expr) {
+  case "Oranges":
+    console.log("Oranges are $0.59 a pound.");
+    break;
+  case "Mangoes":
+  case "Papayas":
+    console.log("Mangoes and papayas are $2.79 a pound.");
+    // Expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
+
+let num =4;
+
+switch(num)
+{
+case (4):
+    console.log("it 4");
+    break;
+  default :
+  console.log("not 4")
+}
