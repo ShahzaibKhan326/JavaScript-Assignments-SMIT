52a87238108fe70f53c918e92d6292d186a3fed3
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-30 11:08:16 -0800

    update Dom

---
+++ Code_Tester_Ground/The_Dom_excersices/app.js
@@ -0,0 +1,17 @@
+// console.log("hello world!");
+
+// --------------------Targeting with childNodes[]---------------------------
+
+// var element = document.childNodes[0].childNodes[1].childNodes[1].childNodes[1];
+// var content = element.innerHTML;
+
+// -------------------------------------------
+
+// var d = document.getElementById("ny");
+// var firstChild = d.childNodes[1];
+// firstChild.style.color="red";
+
+
+// ------------------------------Dom (Document object module--------------------------------------------------
+
+console.log(document);
