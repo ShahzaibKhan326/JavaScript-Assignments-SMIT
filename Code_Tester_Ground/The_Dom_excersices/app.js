7bbaa26ced2f4b396ba89c7a8679f19136f89d16
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-30 11:32:48 -0800

    Added learning Dom_01

--- Code_Tester_Ground/The_Dom_excersices/app.js
+++ Code_Tester_Ground/The_Dom_excersices/app.js
@@ -1,5 +1,8 @@
 // console.log("hello world!");
 
+// <!-- ------------Dom 1.0-------------------------- -->
+
+
 // --------------------Targeting with childNodes[]---------------------------
 
 // var element = document.childNodes[0].childNodes[1].childNodes[1].childNodes[1];
@@ -11,7 +14,9 @@
 // var firstChild = d.childNodes[1];
 // firstChild.style.color="red";
 
+// <!-- ------------Dom 1.0---(End)----------------------- -->
 
-// ------------------------------Dom (Document object module--------------------------------------------------
+
 
 console.log(document);
+console.log(document.body);
