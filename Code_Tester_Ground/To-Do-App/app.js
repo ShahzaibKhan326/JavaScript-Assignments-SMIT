e09d207cbe0417183c334f4e413362bf52850862
ShahzaibKhan326 <kshahzaib07@gmail.com>
2026-01-14 11:25:31 -0800

    Practice my First ToDo app

---
+++ Code_Tester_Ground/To-Do-App/app.js
@@ -0,0 +1,69 @@
+const input = document.querySelector("#taskInput");
+const addBtn = document.querySelector("#addBtn");
+const taskList = document.querySelector("#taskList");
+
+function addTask()
+{
+   const task = input.value.trim();
+
+     if(task === "") return;
+
+     const li = document.createElement("li");
+     li.innerHTML = 
+     `<span class="task-text"> ${task}</span>
+     <span class="delete"> X </span>
+     `
+     ;
+
+     taskList.appendChild(li);
+     input.value = "";
+
+     saveTasks();
+}
+addBtn.addEventListener("click",addTask)
+
+input.addEventListener("keydown",  (e)=>
+{
+    if(e.key === "Enter")
+    {
+        addTask();
+    }
+}
+)
+
+taskList.addEventListener("click", (e) => {
+    if(e.target.classList.contains("delete"))
+    {
+        e.target.parentElement.remove();
+        saveTasks();
+    }
+    if(e.target.classList.contains("task-text"))
+    {
+        e.target.parentElement.classList.toggle("completed")
+        saveTasks();
+    }
+})
+
+function saveTasks()
+{
+    localStorage.setItem("tasks",taskList.innerHTML)
+}
+
+function loadTask()
+{
+    const saved = localStorage.getItem("tasks");
+    if(saved)
+    {
+        taskList.innerHTML = saved;
+    }
+}
+
+const clearBtn = document.querySelector("#clearCompleted");
+
+clearBtn.addEventListener("click", () => {
+  document.querySelectorAll(".completed").forEach(task => task.remove());
+  saveTasks();
+});
+
+
+loadTask();
